39220eaf57db2f965b2fdd1391fac9a5239752e6
lillywillard-beep <lilly.willard@student.csd509j.net>
2026-02-17 09:26:47 -0800

    make donut

--- main.ts
+++ main.ts
@@ -1,21 +1,36 @@
  let mySprite = sprites.create(img`
-     . . . . . . . . . . . . . . . .
-     . . . . . . . . . . . . . . . .
-     . . . . . . . . . b 5 5 b . . .
-     . . . . . . b b b b b b . . . .
-     . . . . . b b 5 5 5 5 5 b . . .
-     . b b b b b 5 5 5 5 5 5 5 b . .
-     . b d 5 b 5 5 5 5 5 5 5 5 b . .
-     . . b 5 5 b 5 d 1 f 5 d 4 f . .
-     . . b d 5 5 b 1 f f 5 4 4 c . .
-     b b d b 5 5 5 d f b 4 4 4 4 b .
-     b d d c d 5 5 b 5 4 4 4 4 4 4 b
-     c d d d c c b 5 5 5 5 5 5 5 b .
-     c b d d d d d 5 5 5 5 5 5 5 b .
-     . c d d d d d d 5 5 5 5 5 d b .
-     . . c b d d d d d 5 5 5 b b . .
-     . . . c c c c c c c c b b . . .
+     ..............bbbbbbb...........
+     ...........bb66663333baa........
+     .........bb3367776333663aa......
+     ........b33333888333389633aa....
+     .......b3333333333333389633aa...
+     ......b34443333333333338633bae..
+     .....b3455433333333334443333ae..
+     ....b33322333dddd3333455233daee.
+     ...b3d333333dd3bbbb33322333dabe.
+     ..b3d333333d3bb33bb33333333da4e.
+     ..bd33333333b33aab3333333223a4ee
+     .b3d3663333b33aab33366332442b4ee
+     .bd3b983333a3aa3333387633ee3b4ee
+     .bd6983333baaa333333387633bb4bee
+     b3d6833333bba333333333863ba44ebe
+     bdd3333333bb3333333333333a44bebe
+     add666633333322333366333ba44bbbe
+     ad67776333332442336983d3a444b4e.
+     add888b333333ee3369833d3a44b44e.
+     add333333333333336833d3a444b4e..
+     a3dd3333344433333dddd3a444b44e..
+     ab33ddd325543333dd33aa444b44e...
+     .eabb3dd32233333baaa4444b44e....
+     .ebabb3d333d33baa444443b44e.....
+     ..ebaab3ddd3aaa4444433b44e......
+     ..eebbaab33a44444333b444e.......
+     ...eeebbaab444b333b4444e........
+     ....ebeeebbbbbbbb4444ee.........
+     .....eebbbb44444444ee...........
+     .......eeebbb444eee.............
+     ..........eeeeee................
+     ................................
  `, SpriteKind.Player)
-
 
 controller.moveSprite(mySprite)
